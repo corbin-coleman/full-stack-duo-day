bdd88b84fb7b9f84dac9eb68575a5aa809cbb7ca
corbin-coleman <corbincol@hotmail.com>
2017-06-08 18:14:09 +0000

    Find the closest two points on a 2D grid given a list/tuple of lists/tuples representing points

--- pair/close_pairs.py
+++ pair/close_pairs.py
@@ -1,12 +1,20 @@
 #!usr/bin/python3
 
 def close_pairs(pairs=[]):
-    if pairs is None:
+    """
+    Find the closest pairs in a list or tuple of tuples or lists
+    Returns a list containg the two closest points
+    Returns False if the incorrect type is passed
+    """
+    if pairs is None or (type(pairs) is not list and type(pairs) is not tuple) or not pairs:
         return False
+    for item in pairs:
+        if type(item) is not list and type(item) is not tuple:
+            return False
     checked_pair = 0
     for point_1 in pairs:
         for point_2 in pairs:
-            if point_1 is point_2:
+            if point_1 is not point_2:
                 side_1 = point_2[0] - point_1[0]
                 side_2 = point_2[1] - point_1[1]
                 distance = (side_1 * side_1 + side_2 * side_2) ** 0.5
